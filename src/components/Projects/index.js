442d97591050e9b31641ea77f1dfb6811f893ff0
Jankat Dinçer <jankatdincer@gmail.com>
2022-09-05 13:13:24 +0300

    Projects animated title and loader added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,7 +5,30 @@ import './index.scss';
 
 const Projects = () => {
 
-    return;
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() =>{
+        return setTimeout(()=>{
+            setLetterClass('text-animate-hover')
+        }, 3000 )
+    }, [])
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={ "Projects".split("")}
+                            idx={15}
+                        />
+                    </h1>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
 }
 
 export default Projects
