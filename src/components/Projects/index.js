dc85738f5459f327a77c916895f090a9c1bc0737
Jankat Dinçer <jankatdincer@gmail.com>
2022-09-05 14:12:42 +0300

    Added Projects index and json

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,29 +2,58 @@ import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import projectsData from '../../data/projects.json';
 
 const Projects = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
+    console.log(projectsData);
 
     useEffect(() =>{
-        return setTimeout(()=>{
+        const timer = setTimeout(()=>{
             setLetterClass('text-animate-hover')
-        }, 3000 )
-    }, [])
+        }, 3000 );
+
+        return()=>{
+            clearTimeout(timer);
+        }
+    });
+
+    const renderProjects = (projects) => {
+        return (
+            <div className="images-container">
+                {
+                    projects.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img src={port.cover} className="projects-image" alt="projects" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 
     return (
         <>
-            <div className='container about-page'>
-                <div className='text-zone'>
-                    <h1>
+            <div className='container projects-page'>
+                    <h1 className="page-title">
                         <AnimatedLetters
                             letterClass={letterClass}
                             strArray={ "Projects".split("")}
                             idx={15}
                         />
                     </h1>
-                </div>
+                    <div>{ renderProjects(projectsData.projects) }</div>
             </div>
             <Loader type="pacman" />
         </>
