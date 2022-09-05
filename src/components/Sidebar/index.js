3c4ec19d3e297fe2e3983fee4e6ebc2a9177950e
Jankat Dinçer <jankatdincer@gmail.com>
2022-09-05 10:02:32 +0300

    Added projects tab to sidebar [WIP]

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,8 +2,8 @@ import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin, faGithub, faYoutube, faSkype } from '@fortawesome/free-brands-svg-icons'
+import { faDesktop, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faYoutube, faSkype, } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -19,6 +19,9 @@ const Sidebar = () => (
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+                <FontAwesomeIcon icon={faDesktop} color="#4d4d4e"/>
             </NavLink>
         </nav>
 
